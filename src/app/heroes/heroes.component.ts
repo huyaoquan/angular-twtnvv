f3d00e562064a3b6e8aeb3acc51cc4e602f44414
huyaoquan <furuki@qq.com>
2020-10-04 20:02:24 +0800

    7 files modified, 12 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+import { Message } from "@angular/compiler/src/i18n/i18n_ast";
+import { Component, OnInit } from "@angular/core";
+import { Hero } from "../hero";
+// import { HEROES } from "../mock-heroes";
+
+import {HeroService} from '../hero.service';
+//import { MessageService } from '../message.service';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
   //hero='Windstorm';
-  hero:Hero={
-    id:1,
-    name:'Windstorm',
-  };
-  constructor() { }
+
+  // hero: Hero = {
+  //   id: 1,
+  //   name: "Windstorm"
+  // };
+
+  //heroes = HEROES;
+
+  heroes : Hero[];
+
+  constructor(
+    private heroService: HeroService,
+    //private messageService: MessageService,
+
+  ) {}
+
+//   getHeroes(): void {
+//   this.heroes = this.heroService.getHeroes();
+// }
+
+getHeroes():void{
+  this.heroService.getHeroes().subscribe(heroes=>this.heroes = heroes);
+}
 
   ngOnInit() {
+    this.getHeroes();
   }
+
+  // selectedHero: Hero;
+
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  // }
+
+
 
 }
